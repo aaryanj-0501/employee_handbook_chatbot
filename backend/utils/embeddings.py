c50ecbd9dedb3ff5979ca7735d401162f5958ac0
aaryanj-0501 <aaryan.j@glideclouds.com>
2026-01-27 17:11:10 +0530

    Fixed code to match HuggingFace version

--- backend/utils/embeddings.py
+++ backend/utils/embeddings.py
@@ -8,7 +8,7 @@ logger=logging.getLogger(__name__)
 load_dotenv()
 
 MODEL_NAME=os.getenv("EMBED_MODEL_NAME")
-embedding_model=HuggingFaceEmbeddings(model=MODEL_NAME)
+embedding_model=HuggingFaceEmbeddings(model_name=MODEL_NAME)
 
 def get_embedding(text:str):
     response=embedding_model.embed_query(text)
